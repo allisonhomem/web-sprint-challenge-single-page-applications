60461b2b1a13841b2daa3fb8cb1318a291926708
Allison Homem <mrs.allisonhomem@outlook.com>
2021-08-20 10:55:17 -0500

    designed home page, attempting route and link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
+import Form from './Form.js';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+    <div className='homepage'>
+      <div className='header'>
+        <h1>Lambda Eats</h1>
+        <Link to='/' id='home-button' component={App}>Home</Link>
+      </div>
+
+      <div className='body_container'> 
+        <h2>Hungry while coding?</h2> 
+        <p>Lambda partners with local artisan pizza shops to deliver delicious food to your door at discounted rates!
+        </p>
+
+        <div className='pizza_pic'>
+          <Link to='/pizza'id='order-pizza' component={Form}>Pizza?</Link>
+        </div>
+      </div>
+     
     </div>
   );
 };
---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import {React} from 'react';
+import { Route } from 'react-router-dom';
+
+const Form = () => {
+    return(
+        <Route path='/pizza'>
+          <form>
+          </form>
+        </Route>
+    );
+}
+
+export default Form;
